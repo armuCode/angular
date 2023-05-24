48184053edb5dc547b3cb53e1d61a58e179169cc
Armando J Bermudez <abermudz@emeal.nttdata.com>
2023-05-23 20:31:34 -0500

    passing and using  Data with event binding AB

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,38 @@
 import { Component } from '@angular/core';
 
+// @Component({
+//   selector: 'app-servers',
+//   templateUrl: './servers.component.html',
+//   styleUrls: ['./servers.component.css'],
+// })
+// export class ServersComponent {}
+
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //selected by itself
+  //selector: '[app-servers]', //selected by attribute
+  //selector: '.app-servers', //selected by class
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server';
+  serverName = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInit() {}
+  onCreateServer() {
+    this.serverCreationStatus = 'server was Created';
+  }
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
